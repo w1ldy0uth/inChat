c4902d05a69f6483c081c367c29e98e344dc88e6
Ivan w1ldy0uth Shurygin <shurygin1vs@gmail.com>
2024-08-11 03:55:56 +0300

    backend: db initial connection

---
+++ server/db/db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+
+func Init() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatalf("Failed to create MongoDB client: %v", err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+
+	fmt.Println("Successfully established connection to MongoDB instance")
+	Client = client
+}
+
+func GetCollection(databaseName, collectionName string) *mongo.Collection {
+	return Client.Database(databaseName).Collection(collectionName)
+}
